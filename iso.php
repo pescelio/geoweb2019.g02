3f00ce5f3ef812bd1bbf5ed84bd3d9d260630587
Elio Pescatore <eliopescatore@Elios-MacBook-Pro.local>
2020-01-01 17:57:09 +0100

    Übergabe Isochrone in DB funktioniert

--- iso.php
+++ iso.php
@@ -1,54 +1,52 @@
-  <?php
+<?php
 
-    // PostgreSQL-Datenbank öffnen (die php-datei liefert Datenbank-Kennung $db)
-    include 'geoweb_pg_open.php';
-    echo 'Die Datenbank '.pg_dbname($db).' wurde erfolgreich geöffnet.';
+  // PostgreSQL-Datenbank öffnen (die php-datei liefert Datenbank-Kennung $db)
+  include 'geoweb_pg_open.php';
+  echo 'Die Datenbank '.pg_dbname($db).' wurde erfolgreich geöffnet.';
 
-    // variable aus js 
-    // $php_iso = json_decode($_POST['res_str']);
-   
+  echo "<------------------------------------------------------------------>";
 
-   // echo"<br />";
+  
+  // $php_iso = json_decode($_POST['res_str']);
+  
 
-  //  foreach ($_POST as $post) {
-  //     echo "Inhalt POST: $post";
-  //  }
+  // echo"<br />";
+  // variable aus js 
+  $php_iso = $_POST['res_php'];
+  echo 'inhalt des übergebenen Arrays: ';
+  print_r ($php_iso);
 
-    $php_iso = $_POST['res_ueb'];
-    print_r ($php_iso);
+  echo "<------------------------------------------------------------------->";
 
-   // echo "<br />";
-   // echo "<br />";
+  $php_iso = json_decode($php_iso);
+  echo 'JSON-Decode: ' . $php_iso;
 
-      
-   // echo var_dump($php_iso);
+  // function console_log($php_iso, $with_script_tags = true) {
+  //   $js_code = 'console.log(' . json_encode($php_iso, JSON_HEX_TAG) . 
+  //   ');';
+  //     if ($with_script_tags) {
+  //         $js_code = '<script>' . $js_code . '</script>';
+  //     }
+  //     echo $js_code;
+  // }
 
-    // function console_log($php_iso, $with_script_tags = true) {
-    //   $js_code = 'console.log(' . json_encode($php_iso, JSON_HEX_TAG) . 
-    //   ');';
-    //     if ($with_script_tags) {
-    //         $js_code = '<script>' . $js_code . '</script>';
-    //     }
-    //     echo $js_code;
-    // }
+  // zuerst json in db geben
+  $insert = "INSERT INTO g02.testisochrone(
+  WITH data AS (SELECT '$php_iso'
+    ::json AS fc)
 
-    // zuerst json in db geben, muss man die variable statt dem jetztjson
-    // $insert = "INSERT INTO g02.testisochrone(
-    // WITH data AS (SELECT '$php_iso'
-    //   ::json AS fc)
+  SELECT
+    row_number() OVER () AS gid,
+    public.ST_AsText(public.ST_GeomFromGeoJSON(feat->>'geometry')) AS geom,
+    feat->'properties' AS properties
+  FROM (
+    SELECT json_array_elements(fc->'features') AS feat
+    FROM data
+  ) AS f)";
 
-    // SELECT
-    //   row_number() OVER () AS gid,
-    //   public.ST_AsText(public.ST_GeomFromGeoJSON(feat->>'geometry')) AS geom,
-    //   feat->'properties' AS properties
-    // FROM (
-    //   SELECT json_array_elements(fc->'features') AS feat
-    //   FROM data
-    // ) AS f)"
+  $result = pg_query($db,$insert) or die ('Fehler bei Speichern: '.pg_last_error($db));
 
-    // $result = pg_query($db,$insert) or die ('Fehler bei Speichern: '.pg_last_error($db));
+  // Datenbank schliessen
+  include 'geoweb_pg_close.php';
 
-    // Datenbank schliessen
-    include 'geoweb_pg_close.php';
-
-    ?>
+  ?>
