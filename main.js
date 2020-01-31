8dd1dd6e9933487529b4a92df3f52dc6c7e95fca
Elio Pescatore <eliopescatore@Elios-MacBook-Pro.local>
2020-01-31 09:04:47 +0100

    ergänzung attribuition stamen

--- main.js
+++ main.js
@@ -75,9 +75,12 @@ const satLayer = new TileLayer({
 //Base Layer von OSM hinzufügen
 const baseLayer = new TileLayer({
   source: new Stamen({
+    attributions: ['Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>.', 'Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://www.openstreetmap.org/copyright">ODbL</a>.'],
+    attributionsCollapsible: false,
     layer: 'toner-lite'
   })
 });
+
 map.addLayer(baseLayer);
 
 // Get the base Sat-Button
